ddeda3fc60e0f3ff275b421fe2e832c1c041fbf1
Your Name <you@example.com>
2017-06-13 20:15:53 +0300

    change btn and add btn

--- js/main.js
+++ js/main.js
@@ -8,5 +8,5 @@ $('.Prices .open-close').on('click', function(){
   $('.Prices').toggleClass('active');
 });
 $('.submit').on('click', function(){
-    alert("Cпасибо за вашу помощь ваше сообщение отправлено мне на почту ");
+    alert("Спасибо за вашу помощь ваше сообщение отправлено и будет рассмотрено в течении 24 часов");
 });
