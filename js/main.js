82a8c96b96abd89689ba088eae44cda095793aa6
Your Name <you@example.com>
2017-06-13 23:55:48 +0300

    add img right content

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $('.feedback .open-close').on('click', function(){
   $('.feedback').toggleClass('active');
 });
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
 $('.Prices .open-close').on('click', function(){
   $('.Prices').toggleClass('active');
+});
+$('.Prices-2 .open-close').on('click', function(){
+  $('.Prices-2').toggleClass('active');
 });
 $('.submit').on('click', function(){
     alert("Спасибо за вашу помощь ваше сообщение отправлено и будет рассмотрено в течении 24 часов");
